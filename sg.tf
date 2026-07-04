1b9a94055a20b1a15c162456299cfda9c5c5e18c
George-Luis <george.lsantos@outlook.com>
2026-07-04 10:51:01 -0300

    Add.

--- sg.tf
+++ sg.tf
@@ -1,9 +1,12 @@
-resource "aws_security_group" "nodeports" {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 30000
-    to_port     = 32768 
-    description = "Allow NodePort access from the internet"
-    protocol    = "tcp"
-    type        = "ingress"
-    security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+resource "aws_vpc_security_group_ingress_rule" "nodeports" {
+  security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+
+  cidr_ipv4 = "0.0.0.0/0"
+
+  from_port = 30000
+  to_port   = 32767
+
+  ip_protocol = "tcp"
+
+  description = "Allow NodePort access from the internet"
 }
